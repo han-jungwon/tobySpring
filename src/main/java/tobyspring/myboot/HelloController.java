00892ec08f6fe150c3bb07c968dff952d94c17ee
wonny2 <rhehf1024@gmail.com>
2023-06-21 12:13:27 +0900

    Initial commit

---
+++ src/main/java/tobyspring/myboot/HelloController.java
@@ -0,0 +1,18 @@
+package tobyspring.myboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+public class HelloController {
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+   public String hello(String name) {
+        return helloService.sayHello(Objects.requireNonNull(name));
+   }
+}
---
+++ src/main/java/tobyspring/myboot/SimpleHelloService.java
@@ -0,0 +1,8 @@
+package tobyspring.myboot;
+
+public class SimpleHelloService implements HelloService {
+    @Override
+    public String sayHello(String name) {
+        return "Hello" + name;
+    }
+}
