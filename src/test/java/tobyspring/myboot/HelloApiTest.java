898b518a7aa082cf58b7f99672885eec98962e99
jungwon <rhehf1024@naver.com>
2023-06-25 23:06:25 +0900

    DI를 이용한 Decorator, Proxy 패턴

--- src/test/java/tobyspring/myboot/HelloApiTest.java
+++ src/test/java/tobyspring/myboot/HelloApiTest.java
@@ -24,7 +24,7 @@ public class HelloApiTest {
         // header(content-type) text/plain
         assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
         // body Hello Spring
-        assertThat(res.getBody()).isEqualTo("HelloSpring");
+        assertThat(res.getBody()).isEqualTo("*HelloSpring*");
     }
 
     @Test
